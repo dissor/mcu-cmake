881e36768baea6ef3310ec45eabed61206454544
guojianwen <guojianwen@foxmail.com>
2022-11-26 16:36:54 +0800

    分离宏定义cmake文件

--- cmake/arm-none-eabi-gcc.cmake
+++ cmake/arm-none-eabi-gcc.cmake
@@ -8,21 +8,25 @@ set(CMAKE_ASM_COMPILER ${GCC_PATH}/${PREFIX}gcc)
 set(CP ${GCC_PATH}/${PREFIX}objcopy)
 set(SZ ${GCC_PATH}/${PREFIX}size)
 
-message(STATUS "${CMAKE_C_COMPILER}")
-
 # 编译选项
 set(CPU "-mcpu=cortex-m3")
 
 # set(FPU "")
 # set(FLOAT-AB "")
 set(MCU "${CPU} -mthumb ${FPU} ${FLOAT-ABI}")
-message(STATUS "${MCU}")
 
 # set(OPT "-Og")
 set(CMAKE_C_FLAGS "${MCU} ${OPT} -Wall -fdata-sections -ffunction-sections -g -gdwarf-2")
 set(CMAKE_C_FLAGS_DEBUG "-g -gdwarf-2")
 set(CMAKE_C_FLAGS_RELEASE "")
-message(STATUS "${CMAKE_C_FLAGS}")
 
 SET(CMAKE_ASM_FLAGS "${MCU} ${OPT} -x assembler-with-cpp -Wall -fdata-sections -ffunction-sections")
-message(STATUS "${CMAKE_ASM_FLAGS}")
+# message(STATUS "${CMAKE_ASM_FLAGS}")
+
+# # 链接脚本
+# set(LDSCRIPT "")
+# set(LIBS "-lc -lm -lnosys")
+# # set(LIBDIR "")
+# set(CMAKE_EXE_LINKER_FLAGS
+#     "${MCU} -specs=nano.specs -T${LDSCRIPT} ${LIBS} ${LIBDIR} -Wl,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map,--cref -Wl,--gc-sections"
+# )
---
+++ cmake/definitions.cmake
@@ -0,0 +1,5 @@
+# 宏定义
+add_definitions(
+    -DUSE_HAL_DRIVER
+    -DSTM32F103xB
+)
