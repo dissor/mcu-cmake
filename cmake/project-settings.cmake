f64ab9067834a00ba631d254cae53716a1d2238f
guojianwen <guojianwen@foxmail.com>
2022-11-30 17:19:41 +0800

    添加 stdio.h 需要的 nosys.speces

--- cmake/project-settings.cmake
+++ cmake/project-settings.cmake
@@ -16,7 +16,7 @@ set(LDSCRIPT ${PROJECT_PATH}/cube/${PROJECT_LINK})
 set(LIBS "-lc -lm -lnosys")
 # set(LIBDIR "")
 set(CMAKE_EXE_LINKER_FLAGS
-    "${MCU} -specs=nano.specs -T${LDSCRIPT} ${LIBS} ${LIBDIR} -Wl,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map,--cref -Wl,--gc-sections"
+    "${MCU} -specs=nano.specs -specs=nosys.specs -T${LDSCRIPT} ${LIBS} ${LIBDIR} -Wl,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map,--cref -Wl,--gc-sections"
 )
 
 # message(STATUS "[${CMAKE_CURRENT_LIST_FILE}](${CMAKE_CURRENT_LIST_LINE}): ${CMAKE_SOURCE_DIR}")
