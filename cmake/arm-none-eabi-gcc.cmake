cc144d04aa2063f28cea75e36288481fca22f6a3
dissor <guojianwenjonas@foxmail.com>
2022-11-30 09:55:26 +0800

    win平台调试正常

--- cmake/arm-none-eabi-gcc.cmake
+++ cmake/arm-none-eabi-gcc.cmake
@@ -1,12 +1,17 @@
 # 工具链
-set(GCC_PATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin")
+# set(GCC_PATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin")
 set(PREFIX "arm-none-eabi-")
-set(CMAKE_C_COMPILER ${GCC_PATH}/${PREFIX}gcc)
-set(CMAKE_ASM_COMPILER ${GCC_PATH}/${PREFIX}gcc)
-
-# 生成工具
-set(CP ${GCC_PATH}/${PREFIX}objcopy)
-set(SZ ${GCC_PATH}/${PREFIX}size)
+if(NOT DEFINED GCC_PATH)
+    set(CMAKE_C_COMPILER ${PREFIX}gcc)
+    set(CMAKE_ASM_COMPILER ${PREFIX}gcc)
+    set(CP ${PREFIX}objcopy)
+    set(SZ ${PREFIX}size)
+else()
+    set(CMAKE_C_COMPILER ${GCC_PATH}/${PREFIX}gcc)
+    set(CMAKE_ASM_COMPILER ${GCC_PATH}/${PREFIX}gcc)
+    set(CP ${GCC_PATH}/${PREFIX}objcopy)
+    set(SZ ${GCC_PATH}/${PREFIX}size)
+endif()
 
 # 编译选项
 set(CPU "-mcpu=cortex-m3")
